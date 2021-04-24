be8a693ab150e6c584a917fc43195824206db564
Aritta <33124040+suho6347@users.noreply.github.com>
2021-04-24 20:51:56 +0900

    Update game.java
    마우스 드래그 시 점으로 끊기던 현상을 선으로 표현되도록 고침

--- 참고자료/3_painting/game.java
+++ 참고자료/3_painting/game.java
@@ -1,32 +1,36 @@
 
 
+
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.awt.event.*;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Color;
-  
-public class game {    
-    
-    public static void main(String[] a) {
+import java.awt.*;
+
+public class game{    
+
+	
+	public static void main(String[] a) {
         
         JFrame f = new JFrame("Canvas");
-        f.setSize(500,500);
+        f.setSize(800,800);
         f.setLocationRelativeTo(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(null);
         f.setVisible( true );
                 
-        BufferedImage bi = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB );
+        BufferedImage bi = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB );
         //BufferedImage: 이미지 픽셀 정보를 버퍼에 저장해 이미지를 처리하는 클래스
         
         JLabel l = new JLabel(new ImageIcon(bi) );
-        l.setBounds(20,20,400,400);
+        l.setBounds(20,20,500,500);
         f.add(l);
         
         // 브러쉬라는 클래스를 생성한다. 아래쪽에 정의함.
         Brush b = new Brush();
-        b.setBounds(20,20,400,400); //크기가 l과 같아야 한다.
+        b.setBounds(20,20,500,500); //크기가 l과 같아야 한다.
         f.add(b);
         
         
@@ -37,13 +41,24 @@ public class game {
                 
                 b.xx=e.getX();
                 b.yy=e.getY();
-                b.repaint();                            
-                b.printAll(  bi.getGraphics() ); //브러쉬를 BufferedImage 에 그린다.                
+                b.repaint();
+                b.printAll(  bi.getGraphics() ); //브러쉬를 BufferedImage 에 그린다.    
+                
+                b.x = b.xx;
+                b.y = b.yy;
             }
             
             public void mouseMoved(MouseEvent e) {
 
             }           
+        });
+        // 마우스 입력 이벤트
+        l.addMouseListener(new MouseAdapter() {
+        
+        	public void mousePressed(MouseEvent e) {
+        		b.x = e.getX();
+        		b.y = e.getY();
+        	}
         });
         
     }   
@@ -51,8 +66,10 @@ public class game {
 
 
 class Brush extends JLabel{
-        
+       
+	public int x, y;
     public int xx, yy;
+    
     public Color col=new Color(255,0,0);
     
     public void paint(Graphics g) {     
@@ -60,5 +77,12 @@ class Brush extends JLabel{
         g.setColor( col );
         g.fillOval( xx-10, yy-10, 20, 20);
         
-    }   
+        //선의 굵기 , 꼭짓점 모양 설정
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(20, 0, 1, 0));
+        
+        //이전 위치와 현재 위치를 선으로 잇기
+        g.drawLine(this.x, this.y, this.xx, this.yy);
+    }
+
 }
