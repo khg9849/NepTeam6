d942558c2f912e3984fbeaa83402944d65b93925
khg9849 <75154470+khg9849@users.noreply.github.com>
2021-05-20 19:53:12 +0900

    Revert "Revert "점찍기, 선그리기, 반짝임제거""
    This reverts commit dcc729106bac320b73ffe8000cad181adebb59b7.

--- untitled_project/src/Brush.java
+++ untitled_project/src/Brush.java
@@ -1,14 +1,18 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.io.Serializable;
 
 import javax.swing.JLabel;
 
 public class Brush  extends JLabel implements Serializable{
 	private static final long serialVersionUID=22L;
-	private int xx,yy;
+	private int xx,yy;	//현재 위치
+	private int x, y;	//바로 이전의 위치
 	private Color col;
 	private int dia;
+	private boolean isMouseDrag;
 	
 	public int getDia() {
 		return dia;
@@ -17,10 +21,19 @@ public class Brush  extends JLabel implements Serializable{
 	public void setDia(int dia) {
 		this.dia = dia;
 	}
+	
+	public boolean get_isMouseDrag() {
+		return isMouseDrag;
+	}
+	
+	public void set_isMouseDrag(boolean t) {
+		this.isMouseDrag = t; 
+	}
 
 	public Brush(Color col, int dia) {
 		this.col = col;
 		this.dia=dia;
+		this.isMouseDrag = false;
 	}
 	
 	public void print() {
@@ -41,6 +54,21 @@ public class Brush  extends JLabel implements Serializable{
 	public void setYy(int yy) {
 		this.yy = yy;
 	}
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 
 	public Color getCol() {
 		return col;
@@ -56,6 +84,17 @@ public class Brush  extends JLabel implements Serializable{
 	
 	public void paint(Graphics g) {
 		g.setColor(col);
-		g.fillOval(xx-dia/2, yy-dia/2, dia, dia);
+		
+		//MousePressed면 점으로 찍기
+		if( this.isMouseDrag == false)
+			g.fillOval(xx-dia/2, yy-dia/2, dia, dia);
+		
+		//MouseDragged면 선으로 잇기
+		if (this.isMouseDrag == true){
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setStroke(new BasicStroke(dia,BasicStroke.CAP_ROUND,0));
+			g.drawLine(x, y, xx, yy);
+		}
+			
 	}
 }
